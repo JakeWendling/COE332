3d0c1c03ededb6803137e29e1fd4e00ea767ae46
Ubuntu <ubuntu@jakew57-vm.js2local>
2023-02-13 21:55:53 +0000

    added docstrings

--- homework03/analyze_water.py
+++ homework03/analyze_water.py
@@ -1,39 +1,62 @@
-#!/usr/bin/env python3
-
 import requests
 import json
 import math
 
-def getJson(url_string):
+def getJson(url_string: str) -> dict:
+    """
+    Retrieve a JSON response from a given URL string and return the result as a dictionary.
+
+    Args:
+        url_string (str): The URL to send a GET request to.
+
+    Returns:
+        dict: The JSON response as a dictionary.
+    """
     response = requests.get(url=url_string)
     return response.json()
 
-def calcTurbidity(a0, I90):
-    '''
-    T = a0 * I90
-    T = Turbidity in NTU Units (0 - 40)
-    a0 = Calibration constant
-    I90 = Ninety degree detector current
-    '''
+def calcTurbidity(a0: float, I90: float) -> float:
+    """
+    Calculate the turbidity in NTU units based on the calibration constant and detector current.
+
+    Args:
+        a0 (float): The calibration constant.
+        I90 (float): The 90-degree detector current.
+
+    Returns:
+        float: The calculated turbidity in NTU units (0-40).
+    """
     T = abs(a0 * I90)
     return T
-    
-def calcMinTime(T0, d, Ts):
-    '''
-    Ts > T0(1-d)**b
-    Ts = Turbidity threshold for safe water
-    T0 = Current turbidity
-    d = decay factor per hour, expressed as a decimal
-    b = hours elapsed
-    '''
+
+def calcMinTime(T0: float, d: float, Ts: float) -> float:
+    """
+    Calculate the minimum time required to return below a safe threshold.
+
+    Args:
+        T0 (float): The current turbidity.
+        d (float): The decay factor per hour, expressed as a decimal.
+        Ts (float): The turbidity threshold for safe water.
+
+    Returns:
+        float: The minimum time required in hours.
+    """
     b = math.log(Ts / T0) / math.log(1 - d)
-    
     return b
 
-def calcTurbidityAverage(turb_data, turb_key, cc_key, dc_key):
-    '''
-    Takes the last five samples and returns the average of the turbdidty
-    '''
+def calcTurbidityAverage(turb_data: dict, turb_key: str, cc_key: str, dc_key: str) -> float:
+    """
+    Calculate the average of the last five samples of turbidity data.
+
+    Args:
+        turb_data (dict): The dictionary containing the turbidity data.
+        turb_key (str): The key of the turbidity data in the dictionary.
+        cc_key (str): The key of the calibration constant in the dictionary.
+        dc_key (str): The key of the detector current in the dictionary.
+
+    Returns:
+        float: The average of the last five samples of turbidity.
+    """
     turbidity_sum = 0.0
     data_count = min(5, len(turb_data[turb_key]))
     for sample in turb_data[turb_key][-5:]:
